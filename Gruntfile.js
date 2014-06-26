907cf0819758808390cc4bc89b837fc767780955
Thomas Coleman <tom@ilikeprograms.com>
2014-06-26 10:06:58 +0100

    [Grunt] Added express, open, and watch tasks to improve dev/build
    The express task enables a localhost server which is automatically run
    when `Grunt` is run and sets the docroot to 'build'.
    
    The Open task will automatically open up a browser tab at http://localhost:9000/build/example.
    
    The Watch task will watch for any changes to the example source files and the ThemeEditor files
    and then automatically lint, ugligy and copy the files to the build directory, and refresh them using livereload.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,11 @@ module.exports = function (grunt) {
 
 		uglify: {
 			build: {
-				files: { // Theme Editor Files
+                // Theme Editor Files
+				files: {
+                    // The Destination file
 					'build/bsThemeEditor-<%= pkg.version %>.min.js': [
+                        // The Source files
 						'src/<%= pkg.name %>/theme-modifier.js',
 						'src/<%= pkg.name %>/branding.js',
 						'src/<%= pkg.name %>/dropdown.js',
@@ -21,24 +24,84 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-		
+        
+        // Create a localhost server to host the example demo
+        express: {
+            all: {
+                // Host on localhost:9000
+                options: {
+                    port: 9000,
+                    hostname: 'localhost',
+                    bases: [__dirname + '/build'], // Set the docroot to be the ./build folder
+                    livereload: true
+                },
+            }
+        },
+        
+        // Automatically open the path when Grunt is run
+        open: {
+            all: {
+                path: "http://localhost:9000/example" // ./build/example (index.html)
+            }
+        },
+        
+        watch: {
+            // Run the tasks when any of the example-src files are changed
+            "example-src": {
+                files: 'example-src/*.*',
+                tasks: ["jshint", "uglify", "copy"],
+
+                options: {
+                  livereload: true
+                }
+            },
+            
+            // Reload the GruntFile.js when it is changed
+            // TODO: Look into getting it to rerun the build process and start
+            // watching again when this is done
+            configFiles: {
+                files: "GruntFile.js",
+                options: {
+                    reload: true
+                }
+            },
+            
+            // Run the tasks when the ThemeEditor JS files change
+            scripts: {
+                files: "src/BootstrapThemeEditor/*",
+                tasks: ["jshint", "uglify", "copy"],
+                options: {
+                    livereload: true,
+                }
+            }
+        },
+
+        // Setup the Copy config to copy all the required files to the build/* folders
 		copy: {
 			main: {
 				files: [
+                    // Copy the Example files
+                    {expand: true, src: "example-src/*", flatten: true, dest: 'build/example/'},
+
 					// JS lib files
 					{expand: true, src: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/jquery/dist/jquery.min.map'], flatten: true, dest: 'build/js/lib'},
 					{expand: true, src: ['bower_components/bootstrap/dist/js/bootstrap.min.js'], flatten: true, dest: 'build/js/lib'},
+                    {expand: true, src: ['bower_components/less.js/dist/less-1.7.3.min.js'], flatten: true, dest: 'build/js/lib'},
 					
-					// Customised Bootstrap
+					// Bootstrap less files
 					{expand: true, src: ['bower_components/bootstrap/less/*'], flatten: true, dest: 'build/less', filter: 'isFile'},
-					
-					{expand: true, src: ['src/bootstrap.less'], flatten: true, dest: 'build/less'},
-					
-					{expand: true, src: ['bower_components/less.js/dist/less-1.7.3.min.js'], flatten: true, dest: 'build/js/lib'},
+					{expand: true, src: ['bower_components/bootstrap/less/*'], flatten: true, dest: 'build/less', filter: 'isFile'},
+                    
+                    // Custom Bootstrap variables file
+					{expand: true, src: 'src/variables-custom.less', flatten: true, dest: 'build/less/'},
 
-					{expand: true, src: ['src/variables-custom.less'], flatten: true, dest: 'build/less'},
+                    // Copy the Custom Bootstrap.less file which adds the theme.less as an import
+                    {expand: true, src: "src/bootstrap.less", flatten: true, dest: 'build/less/'},
 				],
 				options: {
+                    // Replace "variables.less" with "variables-custom.less"
+                    // the variables-custom.less file contains more/altered variables which makes it easier to
+                    // cascade theme changes to multiple dependent elements
 					process: function (content, srcpath) {
 						return content.replace(/variables\.less/g, "variables-custom.less");
 					}
@@ -46,6 +109,7 @@ module.exports = function (grunt) {
 			}
 		},
 
+        // Turn on JShint for the Javascript files in src/BootstrapThemeEditor/
 		jshint: {
 			options: {
 				jshintrc: true,
@@ -54,9 +118,14 @@ module.exports = function (grunt) {
 		}
 	});
 	
+    // Load the Required Tasks
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-express');
+    grunt.loadNpmTasks('grunt-open');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'copy']);
+    // Register the "default" Task
+	grunt.registerTask("default", ["jshint", "uglify", "copy", "express", "open", "watch"]);
 };
