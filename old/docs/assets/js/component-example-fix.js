6e872314cd3037ba25361377c230614037595363
Thomas Coleman <tom@ilikeprograms.com>
2019-03-13 20:34:34 +0000

    Removed all files because this is old and I want a new start

--- old/docs/assets/js/component-example-fix.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('.example-tooltips').tooltip();
-    $('.example-left-tooltip').tooltip({'trigger': 'manual'});
-    $('.example-left-tooltip').tooltip('show');
-
-    $('.example-popovers').popover();
-    $('.example-left-popover').popover({'trigger': 'click'});
-    $('.example-left-popover').popover('show'); 
-});
